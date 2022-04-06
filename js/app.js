742e65ff404d36b9861f9c86f2dc09b1c9d1e231
Aaron <zhangchuqi1998@gmail.com>
2022-04-06 13:51:26 -0500

    finished

---
+++ js/app.js
@@ -0,0 +1,24 @@
+const gallery = document.getElementById('gallery');
+const popup = document.getElementById('popup');
+const selectedImage = document.getElementById('selectedImage');
+const imageIndex = [1,2,3,4,5,6,7,8,9, 10,11,12];
+const selectedIndex = null;
+
+imageIndex.forEach(i => {
+    const image = document.createElement('img');
+    image.src = `/img/img-${i}.jpg`;
+    image.alt = `image ${i}`;
+    image.classList.add('galleryImg');
+    image.addEventListener('click', ()=>{
+        popup.style.transform = 'translateY(0)'
+        selectedImage.src = `/img/img-${i}.jpg`;
+        selectedImage.alt = `img-${i}`;
+    })
+    gallery.appendChild(image);
+})
+
+popup.addEventListener('click', ()=>{
+    popup.style.transform='translateY(-100%)';
+    popup.src = '';
+    popup.alt = '';
+})
